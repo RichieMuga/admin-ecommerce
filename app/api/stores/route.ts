bc787b96fdd6f03978ce862b50510b8da16e9368
Richie Mugambi <richiemugambi4@gmail.com>
2023-09-04 23:57:03 +0300

    refactor: changed unauthorized to unauthenticated

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -13,7 +13,7 @@ export async function POST(
     const { name } = body;
 
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 403 });
+      return new NextResponse("Unauthenticated", { status: 403 });
     }
 
     if (!name) {
