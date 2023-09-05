f923aeed827251a51df1bc49b057b3eee09857d2
Richie Mugambi <richiemugambi4@gmail.com>
2023-09-05 23:34:02 +0300

    feat: added backend data to display on billboards page dynamically

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,22 +6,33 @@ import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { useParams, useRouter } from "next/navigation";
-import prismadb from '@/lib/prismadb';
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from '@/components/ui/data-table';
+import { ApiList } from "@/components/ui/api-list";
 
-const BillboardClient = () => {
+const BillboardClient = ({
+  data
+}:{
+  data: BillboardColumn[]
+}) => {
   const router =useRouter()
   const params = useParams()
 
   return (
     <>
       <div className="flex justify-between items-center">
-        <Heading title={"Billbaords(0)"} description="Manage billboards for your store"/>
+        <Heading title={`Billbaords(${data.length})`} description="Manage billboards for your store"/>
         <Button onClick={()=>router.push(`/${params.storeId}/billboards/new`)}>
           <Plus className="mr-2 h-4 w-4" />
           Add New
         </Button>
       </div>
       <Separator/>
+      <DataTable searchKey="label" columns={columns} data={data}/>
+      <Separator/>
+      <Heading title="API" description="Api calls for billboards"/>
+      <Separator/>
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
