bedc808cf445a19a65e81dfb5745d6d2a8418468
Richie Mugambi <richiemugambi4@gmail.com>
2023-09-12 00:10:11 +0300

    feat: added colors on navbar

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -31,6 +31,11 @@ export const MainNav = ({
       label: "Sizes",
       active: pathname === `/${params.storeId}/sizes`,
     },
+        {
+      href: `/${params.storeId}/colors`,
+      label: "Colors",
+      active: pathname === `/${params.storeId}/colors`,
+    },
     {
       href: `/${params.storeId}/settings`,
       label: "Settings",
